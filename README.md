60167c3b2bfce4fd3c7f4943892e23a067483db8
Pramit Garg <pramitgarg@Pramits-MacBook-Air.local>
2026-01-07 20:59:18 +0530

    Your commit message

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Sentiment-Analysis-using-Simple-RNN
-This project shows the techniques used to analyse the movie review's given by IMDB and using streamlit for Web UI and then deploying it. 
+This project shows the simple RNN used to do text classification of movie review's given by IMDB and using streamlit to display and then deploy it on AWS. 
---
+++ main.py
@@ -0,0 +1,52 @@
+# Step1 : Load all the libraries which are used 
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.models import load_model
+
+# step 2 : Load the IMDB dataset word_index 
+word_index = imdb.get_word_index()
+reverse_word_index =  {value: key for key, value in word_index.items()}
+
+# load the pre-trained moadel with relu activation 
+model = load_model('SimpleRNN_IMDB.h5')
+
+# creating function to decode the reviews 
+def decode_review(encoded_review):
+    return ' '.join([reverse_word_index.get(i - 3, '?') for i in encoded_review])
+
+
+# creating function to preprocess user input
+def preprocess_text(text):
+    words = text.lower().split()
+    encoded_review = [word_index.get(word, 2) + 3 for word in words]
+    padded_review = sequence.pad_sequences([encoded_review],maxlen=500)
+    return padded_review
+
+
+
+import streamlit as st
+## Design streamlit app
+
+st.title('IMDB Movie Review Sentiment Analysis')
+st.write('Enter a movie review to classify it as positive or negative')
+
+
+user_input = st.text_area('Movie review')
+
+# User input 
+
+if st.button('Classify'):
+
+    preprocessed_input= preprocess_text(user_input)
+
+    # make prediction 
+    prediction = model.predict(preprocessed_input)
+    sentiment = 'Postive' if prediction[0][0] > 0.5 else 'Negative'
+
+    # Display the result in streamlit 
+    st.write(f"Sentiment: {sentiment}")
+    st.write(f"Prediction Score : {prediction[0][0]}")
+else: 
+    st.write('Please enter a movie review.')
